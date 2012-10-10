8d5de2e076953df9810fbd861afe419c3a49cb2c
tf <tf@buglab.(none)>
2012-10-10 06:55:46 +0200

    added basic nginx

--- manifests/modules/repos/manifests/init.pp
+++ manifests/modules/repos/manifests/init.pp
@@ -1,9 +1,28 @@
+#should be parameter-ized for maximum DRYness
+
+#class addrepos( $fileName, $fileSource){
+#	file { "/etc/yum/repos.d/$fileName":
+#		owner	=> root,
+#		group	=> root,
+#		mode	=> '0644',
+#		source	=> "$fileSource",
+#	}
+#}
+
 class addrepos { 
 	#yeah....this isn't v. helpful
 	file { '/etc/yum.repos.d/puppetlabs.repo':
-		owner		=> root,
-		group		=> root,
-		mode		=> '0644',
+		owner	=> root,
+		group	=> root,
+		mode	=> '0644',
 		source	=> '/opt/puppet/manifests/modules/repos/files/puppetlabs.repo',
 	}
+	file { '/etc/yum.repos.d/nginx.repo':
+		owner	=> root,
+		group	=> root,
+		mode	=> '0644',
+		source	=> '/opt/puppet/manifests/modules/repos/files/nginx.repo',
+	}
 }
+
+
